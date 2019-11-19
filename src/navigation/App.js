1092ea68f0441d7a9ee72f2b98a0c6fc58a75367
Brandon <brobertstech@gmail.com>
2019-11-19 13:06:05 -0800

    Make details use a modal style

--- src/navigation/App.js
+++ src/navigation/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Button, View, Text } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-const HomeScreen = () => (
+const HomeScreen = ({ navigation }) => (
   <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
     <Text>Home Screen</Text>
+    <Button
+      title="Go to Details"
+      onPress={() => navigation.navigate("Details")}
+    />
+  </View>
+);
+
+const DetailsScreen = () => (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>Details Screen</Text>
   </View>
 );
 
@@ -14,9 +24,14 @@ const AppNavigator = createStackNavigator(
     Home: {
       screen: HomeScreen,
       path: "Home"
+    },
+    Details: {
+      screen: DetailsScreen,
+      path: "Details"
     }
   },
   {
+    mode: "modal",
     initialRouteName: "Home"
   }
 );
