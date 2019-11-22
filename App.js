402083ba4cd631401fe2a1687cf50eea5690b20d
Brandon <brobertstech@gmail.com>
2019-11-22 10:20:29 -0800

    Provide example of SafeAreaView borking the app

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-navigation";
 
 import Navigation from "./src/navigation";
 import ReanimatedBottomSheetApp from "./src/navigation/ReanimatedBottomSheetApp";
+
+// Adding SafeAreaView to the below borks the app
+/*
+<SafeAreaView
+    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+  >
+  <Navigation />
+  </SafeAreaView>
+
+*/
 const App = () => <Navigation />;
 
 export default App;
